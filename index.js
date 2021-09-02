b0eb9ab9ef3c4b026a2b05d9c589badfb0a7c93b
ashleyannlaz <paperpastels@gmail.com>
2021-09-02 14:00:27 -0400

    set up react

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config() // this allows to stash artificial env variables in a file
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const server = express()
@@ -9,6 +10,9 @@ const PORT = process.env.PORT || 5000
 
 server.use(express.json())
 server.use(cors())
+// supply something that will work correctly regardless
+// of environment
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 server.get('/', (req, res) => {
     res.send(`<h1>Web 45 Rocks</h1>`)
@@ -16,6 +20,12 @@ server.get('/', (req, res) => {
 
 server.get('/api', (req, res)=> {
     res.json({message: 'Web 45 is awesome!'})
+})
+
+server.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    // send the build folder from the client
+    // all of this constructs a path to index.html in build
 })
 
 server.listen(PORT,()=> {
