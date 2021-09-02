80ab1c7b7d936b22f96fcf98dd8a85d110bc69a9
ashleyannlaz <paperpastels@gmail.com>
2021-09-02 12:58:12 -0400

    Deployable

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const server = require('express')()
+const express = require('express')
+const cors = require('cors')
+const server = express()
 
-server.listen(5000,()=> {
+const PORT = process.env.PORT || 5000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Web 45 Rocks</h1>`)
+})
+
+server.get('/api', (req, res)=> {
+    res.json({message: 'Web 45 is awesome!'})
+})
+
+server.listen(PORT,()=> {
     console.log('listening on port 5000')
 })
